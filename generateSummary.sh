6f9c5fe6be75ed16ab294c088e2cad74960c6646
Lennart Ochel <lennart.ochel@liu.se>
2017-04-22 13:44:23 +0200

    WIP

--- generateSummary.sh
+++ generateSummary.sh
@@ -2,13 +2,10 @@
 # author: Lennart Ochel
 
 TESTS=$(ls *.mos | rev | cut -c 5- | rev)
-for TEST in $TESTS
-do
-  mkdir -p summary/$TEST/
-done
 
 for TEST in $TESTS
 do
+  mkdir -p summary/$TEST/
   HTML_FILE=summary/$TEST/$TEST.html
   echo "<html><head><title>OpenModelica - Performance Trace Overview</title><body>" > $HTML_FILE
   echo "<h1>OpenModelica - Performance Trace Overview</h1>" >> $HTML_FILE
@@ -33,6 +30,7 @@ do
     for FILE in $FILES
     do
       echo -n "<tr><td><a href=\"https://github.com/OpenModelica/OpenModelica/commit/$(grep OpenModelica- $FILE | head -n1 | grep -o -P '(?<=-g).*')\">$(grep OpenModelica- $FILE | head -n1 | grep -o -P '(?<=OpenModelica-).*')</a></td><td><a href=\"https://github.com/OpenModelica/OMCompiler/commit/$(grep OMCompiler- $FILE | head -n1 | grep -o -P '(?<=-g).*')\">$(grep OMCompiler- $FILE | head -n1 | grep -o -P '(?<=OMCompiler-).*')</a></td>" >> $HTML_FILE
+      echo -n "$(grep OpenModelica- $FILE | head -n1 | grep -o -P '(?<=OpenModelica-).*') " >> temp.dat
       grep "Notification: Performance of $PHASE: time" $FILE | grep -o -E '\-?[0-9]+(,[0-9]+)*(\.[0-9]+(e\-?[0-9]+)?)?( kB| MB| GB| TB)?' | while read LINE
       do
         echo -n "<td>$LINE</td>" >> $HTML_FILE
@@ -48,15 +46,15 @@ do
       echo >> temp.dat
       echo -n "</tr>" >> $HTML_FILE
     done
-    gnuplot -p -e "set title 'time' font ',14' textcolor rgbcolor 'royalblue'; set pointsize 1; set terminal pngcairo size 480,360 enhanced font 'Verdana,10'; set output 'summary/$TEST/$TEST-time-$ID.png'; set yrange [0:*]; plot 'temp.dat' using 1 notitle with linespoints;"
-    gnuplot -p -e "set title 'allocations' font ',14' textcolor rgbcolor 'royalblue'; set pointsize 1; set terminal pngcairo size 480,360 enhanced font 'Verdana,10'; set output 'summary/$TEST/$TEST-allocations-$ID.png'; set yrange [0:*]; plot 'temp.dat' using 3 notitle with linespoints;"
+    gnuplot -p -e "set title 'time' font ',14' textcolor rgbcolor 'royalblue'; set pointsize 1; set terminal pngcairo size 480,360 enhanced font 'Verdana,10'; set output 'summary/$TEST/$TEST-time-$ID.png'; set xtics rotate; set yrange [0:*]; plot 'temp.dat' using 2:xtic(1) notitle with linespoints;"
+    gnuplot -p -e "set title 'allocations' font ',14' textcolor rgbcolor 'royalblue'; set pointsize 1; set terminal pngcairo size 480,360 enhanced font 'Verdana,10'; set output 'summary/$TEST/$TEST-allocations-$ID.png'; set xtics rotate; set yrange [0:*]; plot 'temp.dat' using 4:xtic(1) notitle with linespoints;"
 
     echo "</table>" >> $HTML_FILE
   done
 
   # generate summary plots
-  gnuplot -p -e "set title 'time' font ',14' textcolor rgbcolor 'royalblue'; set pointsize 1; set terminal pngcairo size 480,360 enhanced font 'Verdana,10'; set output 'summary/$TEST/$TEST-time-0.png'; set yrange [0:*]; plot 'temp.dat' using 2 notitle with linespoints;"
-  gnuplot -p -e "set title 'allocations' font ',14' textcolor rgbcolor 'royalblue'; set pointsize 1; set terminal pngcairo size 480,360 enhanced font 'Verdana,10'; set output 'summary/$TEST/$TEST-allocations-0.png'; set yrange [0:*]; plot 'temp.dat' using 4 notitle with linespoints;"
+  gnuplot -p -e "set title 'time' font ',14' textcolor rgbcolor 'royalblue'; set pointsize 1; set terminal pngcairo size 480,360 enhanced font 'Verdana,10'; set output 'summary/$TEST/$TEST-time-0.png'; set xtics rotate; set yrange [0:*]; plot 'temp.dat' using 3:xtic(1) notitle with linespoints;"
+  gnuplot -p -e "set title 'allocations' font ',14' textcolor rgbcolor 'royalblue'; set pointsize 1; set terminal pngcairo size 480,360 enhanced font 'Verdana,10'; set output 'summary/$TEST/$TEST-allocations-0.png'; set xtics rotate; set yrange [0:*]; plot 'temp.dat' using 5:xtic(1) notitle with linespoints;"
 
   echo "</body></html>" >> $HTML_FILE
 done # TEST
