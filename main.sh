bc5ba400c44e443e538868f30dcd6c244acc01fa
Lennart Ochel <lennart.ochel@liu.se>
2017-04-22 11:38:44 +0200

    WIP

---
+++ main.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# author: Lennart Ochel
+
+# clone OpenModelica
+if [ -d "OpenModelica" ]; then
+  git clone https://openmodelica.org/git-readonly/OpenModelica.git OpenModelica
+  cd OpenModelica
+  git submodule update --init --recursive libraries OMCompiler common
+  cd ..
+fi
+
+# find new commits
+cd OpenModelica
+COMMITS=$(git log HEAD..origin/HEAD --format='%H' | tac)
+for COMMIT in $COMMITS
+do
+  echo $COMMIT
+done
