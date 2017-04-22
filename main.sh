0a0967b9fcb83af4e5b96738b8caaa511648aaf3
Lennart Ochel <lennart.ochel@liu.se>
2017-04-22 11:46:19 +0200

    WIP

--- main.sh
+++ main.sh
@@ -11,8 +11,10 @@ fi
 
 # find new commits
 cd OpenModelica
+git fetch --recurse-submodules
 COMMITS=$(git log HEAD..origin/HEAD --format='%H' | tac)
 for COMMIT in $COMMITS
 do
   echo $COMMIT
 done
+cd ..
